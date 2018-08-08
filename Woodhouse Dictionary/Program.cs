0e15a5512b7aaf0dc3b1ed88e1f22d9ef7981d91
JawKnee <mahasacham@yahoo.com>
2018-08-08 10:59:10 -0700

    I got the console to start spitting out definitions and fixed the xml. turns out you cant use Unicode with XML reader

--- Woodhouse Dictionary/Program.cs
+++ Woodhouse Dictionary/Program.cs
@@ -11,19 +11,35 @@ namespace WoodhouseDictionary
     {
         static void Main(string[] args)
         {
-            using (XmlReader reader = XmlReader.Create(@"C:\Users\jpruitt\Source\Repos\Woodhouse-Dictionary\Woodhouse Dictionary\Woodhouse XML\Woodhouse A.xml"))
+            Console.OutputEncoding = Encoding.UTF8;
+            string xmlLocation = @"C:\Users\jpruitt\Source\Repos\Woodhouse-Dictionary\Woodhouse Dictionary\Woodhouse XML\Woodhouse E.xml";
+            using (XmlReader reader = XmlReader.Create(xmlLocation))
             {
                 while (reader.Read())
                 {
-                    if (reader.IsStartElement())
+                    if (reader.IsStartElement() && reader.Name == "English-Entry")
                     {
-                        Console.WriteLine(reader.Name);
+                        Console.WriteLine(reader.GetAttribute("id"));
+                        Console.WriteLine("=========================RECOMMENDATIONS===========================");
+                        Console.WriteLine($"{GetGreekSuggestions(reader.ReadSubtree())}\n");
                     }
                 }
             }
+                Console.ReadKey();
+        }
 
-                Console.WriteLine("Hello World!");
-            Console.ReadKey();
+        static string GetGreekSuggestions(XmlReader wordReader)
+        {
+            string word = "";
+            while (wordReader.Read())
+            {
+                if (wordReader.IsStartElement() && wordReader.Name == "Greek-Entry")
+                {
+                    wordReader.Read();
+                    word = wordReader.Value.Trim();
+                }
+            }
+            return word;
         }
 
         static void GetDictionarySegment()
